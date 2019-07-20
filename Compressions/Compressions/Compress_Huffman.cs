04a2e66894fcf44a432be343e59846d75a2c7e1f
gegegenoge7 <studyingmode62@gmail.com>
2019-07-19 19:43:08 -0800

    added comments

--- Compressions/Compressions/Compress_Huffman.cs
+++ Compressions/Compressions/Compress_Huffman.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/******************************************************************************
+ * Name: Compress_Huffman.cs
+ * Made By: Hayato Yokoyama
+ * This class will be called via compress_with_Huffman, and will compress the 
+ * file via Huffman coding. It will read the input file as a text file and 
+ * convert it into a binary string and output two files: one that has
+ * compressed the file into a binary string and the other that saved the 
+ * Huffman tree as a string.
+******************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,14 +22,18 @@ namespace Compressions
         private byte[] compressed_fileByte;
         private StringBuilder compressed_binary_string;
 
+        /****This method will be called by the form to compress the input file into huffman compressed output file****/
         public void compress_with_Huffman(string InputFilename, string OutputFilename)
         {
+            //we will first count the number of times each character appears in this file.
             frequency_list = new List<char_freq>();
             string fileString = File.ReadAllText(InputFilename);
             create_char_freq(fileString);
-            char_freq root = create_huffman_tree();
-            compress(fileString, root);
-            StringBuilder String_rep_tree = convert_tree();
+
+            char_freq root = create_huffman_tree();//create a huffman tree based off of the frequency of each char in the file
+            compress(fileString, root);//convert the file into binary using the huffman tree
+            StringBuilder String_rep_tree = convert_tree();//convert the tree into a savable format
+            //write the converted binary and huffman tree to appropriate file
             int numBytes = compressed_binary_string.ToString().Length / 8;
             compressed_fileByte = new byte[numBytes];
             for (int i = 0; i < numBytes; ++i)
@@ -30,12 +43,13 @@ namespace Compressions
             File.WriteAllBytes(OutputFilename, compressed_fileByte);
             File.WriteAllText("HuffmanTreeData_" + OutputFilename, String_rep_tree.ToString());
         }
+        /****This method will be called by compress_with_Huffman to create ****/
         private void create_char_freq(string fileString)
         {
             foreach (char c in fileString)
             {
-                bool found = false;
-                foreach (char_freq entry in frequency_list)
+                bool found = false;//saves whether we've seen this character
+                foreach (char_freq entry in frequency_list)//look through the list - if char found, add 1 to count.
                 {
                     if (entry.ID == c)
                     {
@@ -43,19 +57,22 @@ namespace Compressions
                         entry.Count++;
                     }
                 }
-                if (!found)
+                if (!found)//If not, create new entry with that char and count of 1
                 {
                     char_freq newEntry = new char_freq(c);
                     frequency_list.Add(newEntry);
                 }
             }
         }
+        /****This method will be called by compress_with_Huffman to create the huffman tree with the given character frequencies****/
         private char_freq create_huffman_tree()
         {
-            frequency_list = frequency_list.OrderBy(f => f.Count).ToList();
+            frequency_list = frequency_list.OrderBy(f => f.Count).ToList();//list the frequencies by frequency
             char_freq root = new char_freq();
             int index = 0;
             int Num_entries = frequency_list.Count();
+
+            //keep on creating a parent node with the two lowest frequency entries that we haven't visited.
             while (index+1 < Num_entries)
             {
                 char_freq newNode = new char_freq(frequency_list[index], frequency_list[index+1], frequency_list);
--- Compressions/Compressions/Compress_binary_RLE.cs
+++ Compressions/Compressions/Compress_binary_RLE.cs
@@ -19,54 +19,55 @@ namespace Compressions
 {
     class Compress_binary_RLE
     {
+        /****This method will be called from the form to compress the input file into the outputfile****/
         public void compress_with_RLE(string InputFilename, string OutputFilename) {
+            //read input file as bytes
             byte[] fileBytes = File.ReadAllBytes(InputFilename);
             StringBuilder string_fileBytes = new StringBuilder();
             foreach (byte b in fileBytes) {
                 string_fileBytes.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
             }
-            List<int> counted_binary = new List<int>();
+
+            //count the repeating 0 and 1's, but assume that we start from a 0.
+            List<int> counted_binary = new List<int>();//this will be the list of repeating binary - assumes first entry is the number of repeating 0's
             counted_binary.Add(0);
-            counted_binary = compress(string_fileBytes, 0, counted_binary);
+            counted_binary = compress(string_fileBytes, '0', counted_binary);
+
+            //convert the list of repeating binary into number followed by an underscore
             StringBuilder compressed_fileBytes = new StringBuilder();
             foreach (int count in counted_binary) {
                 compressed_fileBytes.Append(count.ToString() + "_");
             }
             File.WriteAllText(OutputFilename, compressed_fileBytes.ToString());
         }
-        private List<int> compress(StringBuilder string_fileBytes, int last_binary, List<int> counted_binary) {
-            if (0 == string_fileBytes.Length)
+
+        /****This recursive method will be called by compress_with_RLE and by itself to convert the binary string representation of the file into a list of numbmer of repeating binary****/
+        private List<int> compress(StringBuilder string_fileBytes, char last_binary, List<int> counted_binary) {
+            if (0 == string_fileBytes.Length)//end condition
             {
                 return counted_binary;
             }
             else {
-                if (last_binary == 0) {
-                    if (string_fileBytes[0] == '0') {
-                        counted_binary[counted_binary.Count - 1] += 1;
-                    }
-                    else {
-                        counted_binary.Add(1);
-                    }
+                if (last_binary == string_fileBytes[0])//if the binary before matches this one
+                {
+                    counted_binary[counted_binary.Count - 1] += 1;//add to ount of repeated binary
                 }
-                else {
-                    if (string_fileBytes[0] == '0')
-                    {
-                        counted_binary[counted_binary.Count - 1] += 1;
-                    }
-                    else
-                    {
-                        counted_binary.Add(1);
-                    }
+                else
+                {
+                    counted_binary.Add(1);//if not, the streak is broken - create new entry to list
                 }
-                return compress(string_fileBytes.Remove(0,1), last_binary, counted_binary);
+                last_binary = string_fileBytes[0];//update last binary
+                return compress(string_fileBytes.Remove(0,1), last_binary, counted_binary);//keep calling the method while shaving off one char in the string at a time
             }
         }
-
+        /****This method will be called from the form to unocmpress the file compressed by this method****/
         public void uncompress_with_RLE(string InputFilename, string OutputFilename)
         {
+            //read all text and split it by the underscore to get an array of string integers
             string fileString = File.ReadAllText(InputFilename);
             string[] fileStringSplit = fileString.Split('_');
 
+            //build the binary representation of the file by keep on appending the appropriate binary for each count
             StringBuilder UncompressedFileString = new StringBuilder();
             bool binaryBool = false;
             foreach (string entry in fileStringSplit)
@@ -79,6 +80,8 @@ namespace Compressions
                 }
                 binaryBool = !binaryBool;
             }
+
+            //convert the binary string into array of bytes
             int numBytes = UncompressedFileString.ToString().Length / 8;
             byte[] Uncompressed_fileByte = new byte[numBytes];
             for (int i = 0; i < numBytes; ++i)
